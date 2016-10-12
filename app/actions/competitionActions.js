960d577d83a0e153c26d7b92fa51a9ab3baa6fa1
Carlos Fernandez Jimenez <cfdez89@gmail.com>
2016-10-12 16:09:30 -0600

    Se crea modulo para ejecutar las acciones sobre una competición

---
+++ app/actions/competitionActions.js
@@ -0,0 +1,20 @@
+/** 
+ * App for emit vote to projects in Fundatec
+ * By Carlos Fernandez Jimenez
+ * Using ES5, React, McFly(flux), Bootstrap
+ * This module is for define competitions actions
+ */
+
+/** import libs dependencies */
+var Flux = new McFly();
+
+var CompetitionActions = Flux.createActions({
+    addCompetition: function(data){
+       return {
+          actionType: "ADD_COMPETITION",
+          data: data
+       }
+    }
+});
+
+module.exports = CompetitionActions;
