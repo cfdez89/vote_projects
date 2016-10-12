b78f4a38d8d9b70c77f625aaafef6a424c1aeb0e
Carlos Fernandez Jimenez <cfdez89@gmail.com>
2016-10-12 02:31:34 -0600

    Se define las acciones para el usuario

---
+++ app/actions/userActions.js
@@ -0,0 +1,20 @@
+/** 
+ * App for emit vote to projects in Fundatec
+ * By Carlos Fernandez Jimenez
+ * Using ES5, React, McFly(flux), Bootstrap
+ * This module is for define user actions
+ */
+
+/** import libs dependencies */
+var Flux = new McFly();
+
+var UserActions = Flux.createActions({
+    addUser: function(data){
+       return {
+          actionType: "ADD_USER",
+          data: data
+       }
+    }
+});
+
+module.exports = UserActions;
