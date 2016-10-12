212c4a1aa8ab918ba8d5eaed2136170fc07a8d39
Carlos Fernandez Jimenez <cfdez89@gmail.com>
2016-10-12 16:12:40 -0600

    Modulo para constantes del app

---
+++ app/constants/userConstants.js
@@ -0,0 +1,13 @@
+/** 
+ * App for emit vote to projects in Fundatec
+ * By Carlos Fernandez Jimenez
+ * Using ES5, React, McFly(flux), Bootstrap
+ * This module is for define user constant to dispatch actions
+ */
+
+/** import libs dependencies */
+
+
+module.exports = {
+    ADD_USER:'ADD_USER'
+}
