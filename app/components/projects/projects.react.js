088c224079e62b7a8d776103155acfd81105e4dc
Carlos Fernandez Jimenez <cfdez89@gmail.com>
2016-10-26 01:37:31 -0600

    Se actualiza componente para mostrar los proyectos

--- app/components/projects/projects.react.js
+++ app/components/projects/projects.react.js
@@ -2,14 +2,14 @@
  * App for emit vote to projects in Fundatec
  * By Carlos Fernandez Jimenez
  * Using ES5, React, McFly(flux), Bootstrap
- * This module is for show all projects by competition
+ * This module is for show all competition projects 
  */
 
 /** import libs dependencies */
 var React = require('react');
 
 /** import components */
-var CompetitionList = require('../competitions/competitionList.react');
+var ProjectList = require('../projects/projectList.react');
 
 /** import bootstrap component modules */
 var Tabs = require('react-bootstrap/lib/Tabs');
@@ -17,33 +17,35 @@ var Tab = require('react-bootstrap/lib/Tab');
 
 /** component global data */
 var _data = {
-  competitions: [
+  projects: [
     {
       id:1,
+      competitionId:1,
       title: 'Electronic Competition',
       description: 'Find and share new ideas related to electronics.',
       start_date: '03/11/2016',
-      picture: 'app/images/electronic.jpeg'
+      picture: '/projects/app/images/electronic.jpeg'
     },
     {
       id:2,
+      competitionId:2,
       title: 'Chemistry Competition',
       description: 'Find and share new ideas related to chemistry.',
       start_date: '12/12/2016',
-      picture: 'app/images/chemistry.jpeg'
+      picture: '/projects/app/images/chemistry.jpeg'
     }
   ]
 };
 
 function getProjectsState(){
-  return _data.competitions;
+  return _data.projects;
 }
 
 /** projects component */
 var Projects = React.createClass({
     getInitialState() {
         return {
-            competitions: getProjectsState()
+            projects: getProjectsState()
         }
     },
     setStyles: function(){
@@ -54,14 +56,10 @@ var Projects = React.createClass({
     render() {
         return (
             <div style={this.setStyles()}>
-                <h1>Project</h1>
-                <br></br>
-                <h5>des</h5>
+                <h1>Projects</h1>
                 <hr></hr>
-                
-           
+                <ProjectList data={this.state.projects}/>
             </div>
-        
         );
     }
 });
