0bc26c199d372cbbee5b8d8eef330761370d81ef
Carlos Fernandez Jimenez <cfdez89@gmail.com>
2016-10-26 01:38:14 -0600

    Se agregan etiquetas para el proyecto

--- app/components/projects/projectItem.react.js
+++ app/components/projects/projectItem.react.js
@@ -16,19 +16,18 @@ var Image = require('react-bootstrap/lib/Image');
 var Col = require('react-bootstrap/lib/Col');
 var Button = require('react-bootstrap/lib/Button');
 
-/* competition item component */
-var CompetitionItem = React.createClass({
+/* project item component */
+var ProjectItem = React.createClass({
     render:function(){
         return (
             <Col xs={12} md={6} lg={4}>
                 <Thumbnail>
                     <Image src={this.props.data.picture} alt="242x200" rounded />
                     <br></br>
-                    <h3>{this.props.data.title}</h3>
-                    <p>Description: {this.props.data.description}</p>
-                    <p>Start date at: {this.props.data.start_date}</p>
+                    <h3>{this.props.data.name}</h3>
+                    <p>Description: {this.props.data.short_description}</p>
                     <span>
-                        <Link to={'/competitions/'+this.props.data.id+'/projects' }>View projects</Link>
+                        <Link to={'/competitions/'+this.props.data.competitionId+'/projects/'+this.props.data.id}>View project</Link>
                     </span>  
                 </Thumbnail>
              </Col>
@@ -36,42 +35,4 @@ var CompetitionItem = React.createClass({
     }
 });
 
-module.exports = CompetitionItem;/** 
- * App for emit vote to projects in Fundatec
- * By Carlos Fernandez Jimenez
- * Using ES5, React, McFly(flux), Bootstrap
- * This module is for show specified competition
- */
-
-/** import libs dependencies */
-var React = require('react');
-var ReactRouter = require('react-router');
-var Link = ReactRouter.Link;
-
-/** import bootstrap component modules */
-var Thumbnail = require('react-bootstrap/lib/Thumbnail');
-var Image = require('react-bootstrap/lib/Image');
-var Col = require('react-bootstrap/lib/Col');
-var Button = require('react-bootstrap/lib/Button');
-
-/* competition item component */
-var CompetitionItem = React.createClass({
-    render:function(){
-        return (
-            <Col xs={12} md={6} lg={4}>
-                <Thumbnail>
-                    <Image src={this.props.data.picture} alt="242x200" rounded />
-                    <br></br>
-                    <h3>{this.props.data.title}</h3>
-                    <p>Description: {this.props.data.description}</p>
-                    <p>Start date at: {this.props.data.start_date}</p>
-                    <span>
-                        <Link to={'/competitions/'+this.props.data.id+'/projects' }>View projects</Link>
-                    </span>  
-                </Thumbnail>
-             </Col>
-        )
-    }
-});
-
-module.exports = CompetitionItem;
+module.exports = ProjectItem;
