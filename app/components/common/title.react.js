f45032895e8b127e60477fbc8b698c6e1285a01a
Carlos Fernandez Jimenez <cfdez89@gmail.com>
2016-10-26 02:02:13 -0600

    Componente para encabezados

---
+++ app/components/common/title.react.js
@@ -0,0 +1,29 @@
+/** 
+ * App for emit vote to projects in Fundatec
+ * By Carlos Fernandez Jimenez
+ * Using ES5, React, McFly(flux), Bootstrap
+ * This module is for show main title page
+ */
+
+/** import libs dependencies */
+var React = require('react');
+
+/** title component */
+var Title = React.createClass({
+    setStyles: function(){
+        return {
+            textAlign: 'center'
+        }
+    },
+    render() {
+        return (
+            <div style={this.setStyles()}>
+                <h1>{this.props.title}</h1>
+                <hr></hr>
+                <br></br>
+            </div>
+        );
+    }
+});
+
+module.exports = Title;
